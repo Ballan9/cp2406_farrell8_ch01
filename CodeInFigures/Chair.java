6d685f93d89f97c795d603459cda7995c8199a96
Ballan9 <allan.serrurier@my.jcu.edu.au>
2017-08-04 10:16:49 +1000

    Added chair class

---
+++ CodeInFigures/Chair.java
@@ -0,0 +1,13 @@
+public class Chair
+{
+    public static void main(String[] args) {
+        System.out.println("x                 x");
+        System.out.println("x                 x");
+        System.out.println("x                 x");
+        System.out.println("x    xxxxxxxxx    x");
+        System.out.println("xxxx x       x xxxx");
+        System.out.println("x  x x       x x  x");
+        System.out.println("x  x x       x x  x");
+        System.out.println("x  x x       x x  x");
+    }
+}
